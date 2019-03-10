6505305a5019fd62b5556480d80246016d2716fc
Anirudh Katti <kattianirudh@gmail.com>
2019-03-10 23:43:54 +0530

    Add header.js

---
+++ src/components/Header.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import "./Header.css";
+
+class Header extends Component {
+  render() {
+    return (
+        <div className="height-100 left-pane">
+          <div className="name-div">
+            <h1 className="names">Hello, Anirudh</h1>
+            <p className="username">@kattianirudh</p>
+          </div>
+          <div className="details" style={{ height: "88vh" }}>
+            <div className="workspace-list-div">
+              <div className="workspace-list">
+                <span>GN</span>
+                <span>WS1</span>
+                <span>WS2</span>
+                <span>RN</span>
+                <span>AB</span>
+                <span>XZ</span>
+              </div>
+            </div>
+
+            <div className="channel-details-div">
+              <div className="channels">
+                <h2 className="channel-header">Channels</h2>
+                <div className="channel-list">
+                  <div
+                    target="message-iframe"
+                    className="channel-name nope "
+                    method="POST"
+                  >
+                    <p />#<p className="names">general</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    );
+  }
+}
+
+export default Header;
