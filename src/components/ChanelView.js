dbf70e20cc109b9cb2359dadaa8bc2c40cc9da60
Anirudh Katti <kattianirudh@gmail.com>
2019-03-10 23:44:57 +0530

    Add ChanelView.js

---
+++ src/components/ChanelView.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import ViewHeader from "./ViewHeader";
+import './ChanelView.css';
+
+//This is the component that includes the workplace name
+class ChanelView extends Component {
+  render() {
+    return (
+        <ViewHeader />
+    );
+  }
+}
+
+export default ChanelView;
