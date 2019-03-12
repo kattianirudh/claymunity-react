5fe841226296e09801c4ac9d9f148f58c92098cd
pavanpej <pavanpej@gmail.com>
2019-03-12 17:56:33 +0530

    add a container for all messages

--- src/components/MessagesContainer.js
+++ src/components/MessagesContainer.js
@@ -1,27 +1,17 @@
 import React, { Component } from "react";
 import "./MessagesContainer.css";
+import MessageComponent from "./MessageComponent";
 
-class Messages extends Component {
+class MessagesContainer extends Component {
   render() {
     return (
       <div className="messagess-div">
         <div className="messagess-box" id="messages-box">
-          <div className="message-div">
-            <div className="display-pic">AK</div>
-            <div className="right-pane">
-              <div className="details">
-                <h3 className="name">kattianirudh</h3>
-                <p className="text-muted">6:00</p>
-              </div>
-              <div className="message-text">
-                <p>Hi</p>
-              </div>
-            </div>
-          </div>
+          <MessageComponent />
         </div>
       </div>
     );
   }
 }
 
-export default Messages;
+export default MessagesContainer;
